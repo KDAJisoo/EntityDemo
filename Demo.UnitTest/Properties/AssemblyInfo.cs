32683d2e97c45aaf42740ed62404621b43a68ac7
DESKTOP-SAE8AE1\84988 <qquang51@gmail.com>
2021-01-05 08:38:19 +0700

    first commit!

---
+++ Demo.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Demo.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Demo.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c01d2dde-8ac3-4f9e-8ffa-4efb34681b0b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Demo.data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Demo.data
+{
+    public class Class1
+    {
+    }
+}
